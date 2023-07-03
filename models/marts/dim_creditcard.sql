da25d102fe71387cf192a8c8ea2ab9a67784a87d
Hingryd Lima Rauen <hingryd@indicium.tech>
2023-07-03 15:05:00 -0300

    update project

--- models/marts/dim_creditcard.sql
+++ models/marts/dim_creditcard.sql
@@ -1,14 +1,21 @@
-SELECT
-    MD5(CAST(creditcardid AS string)) AS creditcard_sk,
-    card_type,
-    card_number,
-    expmonth,
-    expyear,
-    current_timestamp AS modified_date
-FROM (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (PARTITION BY creditcardid ORDER BY creditcardid) AS remove_duplicates_index
-    FROM {{ ref('int_creditcard') }}
-) AS creditcard
-WHERE remove_duplicates_index = 1;
+with
+    creditcard as (
+        select *
+        from {{ ref('int_creditcard_dim') }}
+    ),
+    remove_duplicates as (
+        select
+            *,
+            row_number() over (partition by  creditcardid order by creditcardid) as remove_duplicates_index,
+        from creditcard
+    ),
+    creditcard_sk as (
+        select
+            MD5(cast(creditcardid as string)) as creditcard_sk,
+            salesorderid,
+            cardtype,
+        from remove_duplicates
+        where remove_duplicates_index = 1
+    )
+select *
+from creditcard_sk
