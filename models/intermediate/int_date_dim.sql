2138c95c3206b4ac1931049d75e25c2dc8d60579
Hingryd Lima Rauen <hingryd@indicium.tech>
2023-07-04 19:07:57 -0300

    delete intermediate repo

--- models/intermediate/int_date_dim.sql
+++
@@ -1,9 +0,0 @@
-with 
-    salesorderheader as (
-        select
-            salesorderid
-            , orderdate
-        from {{ ref('int_salesorderheader') }} 
-)
-select *
-from salesorderheader
